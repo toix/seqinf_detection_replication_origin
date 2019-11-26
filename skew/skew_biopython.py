8d6bd162925cd6efe45270f9cbeb4d374e0a7ce6
StatistNr27 <proured47@gmail.com>
2019-11-26 23:57:06 +0100

    Added hamming distance for whole sequence without any fancy stuff (really slow)

--- skew/skew_biopython.py
+++ skew/skew_biopython.py
@@ -1,25 +1,61 @@
 from Bio import SeqIO as seqio
 from Bio import SeqUtils as seq
 import matplotlib.pyplot as plt
-from PIL import ImageGrab
 from inspect import signature
 
 ecoli = seqio.read('ecoli.fasta', 'fasta')
+
+
+
+dnaa = "TTATNCACC"
+
+
+def similarity(char1, char2):
+    if char1 == char2:
+        return 1
+    return 0
+
+
+def hamming_dist(seq1, seq2):
+    score = 0
+    for i in range(len(seq1)):
+        score += similarity(seq1[i], seq2[i])
+    return score
+
+
+distances = []
+for i in range(len(ecoli.seq)):
+    print(i)
+    if i+len(dnaa)-1 < len(ecoli.seq):
+        distances.append(hamming_dist(dnaa, ecoli.seq[i:i+len(dnaa)]))
+    else:
+        distances.append(hamming_dist(dnaa, ecoli.seq[i:]+ecoli.seq[:(i+len(dnaa))%len(ecoli.seq)]))
+
+
+
 print(ecoli)
 print('Length of genome: ' + str(len(ecoli.seq)))
 print('Function GC_skew:\n' + seq.GC_skew.__doc__)
-skew = seq.GC_skew(ecoli.seq)
+skew = seq.GC_skew(ecoli.seq, window=500)
 print('Arguments: ' + str(signature(seq.GC_skew)))
 # seq.xGC_skew(ecoli.seq)
-fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 5))
+#fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 5))
+fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=False, figsize=(20, 5))
+# ax1
 pos = [x for x in range(1,len(skew)+1)]
 ax1.plot(pos, skew)
+
+# ax2
 y = 0
 skew_acc = []
 for s in skew:
     y += s
     skew_acc.append(y)
 ax2.plot(pos, skew_acc)
+
+# ax3
+ax3.plot(range(len(ecoli.seq)), distances)
+
 ax2.set_xlabel('Position')
 ax1.set_ylabel('GC skew')
 ax2.set_ylabel('cumulative GC skew')
@@ -28,7 +64,9 @@ for ax in [ax1, ax2]:
 plt.suptitle('GC skew')
 print('Position of minimum (OriC): ' + str(skew_acc.index(min(skew_acc))))
 # fig.savefig('plot_skew.pdf')
-# plt.show()
+
+plt.show()
+#from PIL import ImageGrab
 # im2 = ImageGrab.grab(bbox=None)
 # im2.show()
 
