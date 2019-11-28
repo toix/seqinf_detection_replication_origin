d28e0c49cebbc3096d43ace802893aa1b35b05c7
StatistNo27 <statistn27@gmail.com>
2019-11-28 01:19:24 +0100

    added regional calculation of the similarity to the motif, code is still messy

--- skew/skew_biopython.py
+++ skew/skew_biopython.py
@@ -2,12 +2,13 @@ from Bio import SeqIO as seqio
 from Bio import SeqUtils as seq
 import matplotlib.pyplot as plt
 from inspect import signature
+import numpy as np
 
 ecoli = seqio.read('ecoli.fasta', 'fasta')
 
 
 
-dnaa = "TTATNCACC"
+dnaa = "TTATNCACA"
 
 
 def similarity(char1, char2):
@@ -23,14 +24,27 @@ def hamming_dist(seq1, seq2):
     return score
 
 
-distances = []
-for i in range(len(ecoli.seq)):
-    print(i)
-    if i+len(dnaa)-1 < len(ecoli.seq):
-        distances.append(hamming_dist(dnaa, ecoli.seq[i:i+len(dnaa)]))
-    else:
-        distances.append(hamming_dist(dnaa, ecoli.seq[i:]+ecoli.seq[:(i+len(dnaa))%len(ecoli.seq)]))
+def motif_distances(seq, motif):
+    distances = []
+    for i in range(len(seq)):
+        print(i)
+        if i+len(motif)-1 < len(seq):
+            distances.append(hamming_dist(motif, seq[i:i+len(motif)]))
+        else:
+            distances.append(hamming_dist(motif, seq[i:]+seq[:(i+len(motif))%len(seq)]))
+    return distances
 
+def min_region(seq, min_skew_id, region_size):
+    dev = int(region_size/2)
+
+    if min_skew_id - dev >= 0 and min_skew_id + dev <= len(seq):
+        return seq[min_skew_id-dev:min_skew_id+dev]
+
+    if min_skew_id - dev < 0:
+        return seq[min_skew_id - dev:] + seq[:min_skew_id + dev]
+
+    if min_skew_id + dev > len(seq):
+        return seq[min_skew_id - dev:] + seq[:min_skew_id+dev - len(seq)]
 
 
 print(ecoli)
@@ -42,7 +56,7 @@ print('Arguments: ' + str(signature(seq.GC_skew)))
 #fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(20, 5))
 fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=False, figsize=(20, 5))
 # ax1
-pos = [x for x in range(1,len(skew)+1)]
+pos = [x*500 for x in range(1,len(skew)+1)]
 ax1.plot(pos, skew)
 
 # ax2
@@ -53,14 +67,23 @@ for s in skew:
     skew_acc.append(y)
 ax2.plot(pos, skew_acc)
 
+
+regionsize = 4000
+half_regionsize = int(regionsize/2)
+
 # ax3
-ax3.plot(range(len(ecoli.seq)), distances)
+oriC_mid = np.argmin(np.array(skew_acc))
+search_region = min_region(ecoli.seq, oriC_mid*500, regionsize)
+scores = motif_distances(search_region, dnaa)
+ax3.plot(range(oriC_mid-half_regionsize, oriC_mid+half_regionsize), scores)
+
+ax3.set_xlim(oriC_mid-half_regionsize, oriC_mid+half_regionsize)
 
 ax2.set_xlabel('Position')
 ax1.set_ylabel('GC skew')
 ax2.set_ylabel('cumulative GC skew')
 for ax in [ax1, ax2]:
-    ax.set_xlim(0, len(skew_acc)+1)
+    ax.set_xlim(0, (len(skew_acc)+1)*500)
 plt.suptitle('GC skew')
 print('Position of minimum (OriC): ' + str(skew_acc.index(min(skew_acc))))
 # fig.savefig('plot_skew.pdf')
